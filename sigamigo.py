2b75203e1b679e8ded972344f3f4fcc195580550
mahmoud. <161757573+donttesme@users.noreply.github.com>
2024-10-12 15:40:22 +0300

    Update sigamigo.py

--- sigamigo.py
+++ sigamigo.py
@@ -22,7 +22,7 @@ async def check_pass(username):
 
      header={
      'Host': 'enum.thm',
-     'User-Agent':'Mozilla/5.0 (X11; Linux x86_64; rv:109.0) Gecko/20100101 Firefox/115.0',
+     'User-Agent': 'Mozilla/5.0 (X11; Linux aarch64; rv:102.0) Gecko/20100101 Firefox/102.0',
      'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8',
      'Accept-Language': 'en-US,en;q=0.5',
      'Accept-Encoding': 'gzip, deflate, br',
